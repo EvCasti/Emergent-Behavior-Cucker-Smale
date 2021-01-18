8070a90eae84ccc3a74d61fdaa250545384720d9
OVlasiuk <oleksandr.vlasiuk@gmail.com>
2021-01-18 14:56:07 -0500

    Add commented out lines with hold on/off

--- chicken_scratch.m
+++ chicken_scratch.m
@@ -1,37 +1,37 @@
-% plotting sequences of points
+%% TODO list
+% - visualize better
+% - optimize: remove all the loops in k (? bsxfun)
+% - study flocks on surfaces: instead of putting birds in the plane,
+% we would put them on a surface: torus, sphere, etc
+
 %% constants
 K = 200;
 sig = 1;
-beta = .2;
+beta = .3;
 
-% - visualize better
-% - optimize: remove all the loops in k
-% - study flocks on surfaces: instead of putting birds in the plane,
-% we would put them on a surface: torus, sphere, etc
-%% Initialization :Random positions, Random Velocities
+%% Initialization: Random positions, Random Velocities
 dim = 2;
-k = 20; % number of birds in a flock
- % velocities of the birds
-v_new = randn(dim,k);  % new velocities
-pos_new = 1000*rand(dim,k); % positions of birds
- % positions of birds
-deltat = 2e-1; % time step
+k = 80;                               % number of birds in a flock
+L = 100;                               % size of the starting box
+v_new = randn(dim,k);                 % initial velocities
+pos_new = L*rand(dim,k);              % initial positions of birds
+deltat = 1e-2;                        % time step
 eta = @(x,K,beta) K/(sig^2 + x)^beta;
-T = 10; %T = time stopped
+T = 10;                               % stopping time
 
 %% Matrix A
 A = zeros(k, k);
 
-%% main loop: t =0 while (t < 1 ): 
-% t = t + deltat
+%% main loop
 t=0;
+% hold on;
 while (t<T)
     t = t+deltat;
     pos = pos_new;
     for i=1:k
-    for j =1:k
-        A(i,j) = eta( (norm(pos(:,i)-pos(:,j)))^2 , K, beta);
-    end
+        for j =1:k
+            A(i,j) = eta( (norm(pos(:,i)-pos(:,j)))^2 , K, beta);
+        end
     end
 
 
@@ -45,6 +45,16 @@ end
 
 %% Update positions using first eqn in (4)
 pos_new = pos +deltat*v;
+% if (t > 0.9 * T)
+%     quiver (pos(1,:), pos(2,:),  deltat*v(1,:), deltat*v(2,:) );
+% end
+
 end
-quiver (pos_new(1,:), pos_new(2,:),  v_new(1,:), v_new(2,:));
-% plotflock(pos);
+% hold off;
+
+%% Subtract the mean from positions
+% pos_new = pos_new - mean(pos_new,2);
+
+quiver (pos(1,:), pos(2,:),  deltat*v(1,:), deltat*v(2,:));
+% quiver (zeros(size(pos_new(1,:))), zeros(size(pos_new(2,:))),  v_new(1,:), v_new(2,:),0);
+
